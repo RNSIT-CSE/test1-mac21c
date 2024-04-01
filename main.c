5674e37d3859715842672f5df5b5c22f79bc3298
mac21c <165639251+mac21c@users.noreply.github.com>
2024-04-01 06:44:37 +0000

    tes

--- main.c
+++ main.c
@@ -1,2 +1,22 @@
 // write a program to add 2 numbers
 //write your code here
+#include <stdio.h>
+
+int main() {
+    int number1, number2, sum;
+
+    // Ask for input
+    printf("Enter first number: ");
+    scanf("%d", &number1);
+
+    printf("Enter second number: ");
+    scanf("%d", &number2);
+
+    // Calculate the sum
+    sum = number1 + number2;
+
+    // Display the result
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
